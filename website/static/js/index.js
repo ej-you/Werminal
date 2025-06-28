23b20518eeaf17e54af7cecbee13010dff625705
Danil-114195722 <ejyou.user@gmail.com>
2025-06-28 21:59:53 +0300

    Added ui features

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -1,3 +1,5 @@
+let term /* Terminal */ = null;
+let ws /* WebSocket */ = null;
 // получение HTML элемента для терминала
 const terminalElement = function () {
     let elem = document.getElementById('terminal');
@@ -8,15 +10,52 @@ const terminalElement = function () {
     return elem
 }()
 
-// создание терминала
-const term = NewTerminal(terminalElement)
-SetupTerminalFocus(terminalElement, term)
+function closeTerm() {
+    terminalElement.style.display = "none"
+    term.dispose()
+    term = null
 
-// открытие WebSocket соединения
-const ws = NewWebSocket(term.rows, term.cols)
+    document.getElementById('new-session').style.display = "block"
+    document.getElementById('close-session').style.display = "none"
+}
 
-// настройка обработчиков для ws и терминала
-SetupOnMessage(ws, term)
-SetupOnError(ws, term)
-SetupOnClose(ws, term)
-SetupOnKey(ws, term)
+function StopTerminal() {
+    console.log("stop terminal")
+    if (term === null) {
+        return
+    }
+    terminalElement.style.display = "none"
+
+    // закрываем WebSocket соединение и терминал
+    ws.close(1000)
+    closeTerm()
+}
+
+function StartTerminal() {
+    console.log("start terminal")
+    if (term !== null) {
+        return
+    }
+    terminalElement.style.display = "block"
+
+    // создание терминала
+    term = NewTerminal(terminalElement)
+    SetupTerminalFocus(terminalElement, term)
+
+    // открытие WebSocket соединения
+    ws = NewWebSocket(term.rows, term.cols)
+
+    // настройка обработчиков для ws и терминала
+    SetupOnMessage(ws, term)
+    SetupOnError(ws, term)
+    SetupOnClose(ws, term, closeTerm)
+    SetupOnKey(ws, term)
+
+    document.getElementById('new-session').style.display = "none"
+    document.getElementById('close-session').style.display = "block"
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('new-session').addEventListener('click', StartTerminal);
+    document.getElementById('close-session').addEventListener('click', StopTerminal);
+});
--- website/static/js/terminal/ws.js
+++ website/static/js/terminal/ws.js
@@ -1,11 +1,13 @@
 // import { Terminal } from "@xterm/xterm";
 
 // обработка закрытия websocket соединения
-function SetupOnClose(socket /* WebSocket */, term /* Terminal */) {
+function SetupOnClose(socket /* WebSocket */, term /* Terminal */, closeFunc /* function */) {
     socket.onclose = (event /* CloseEvent */) => {
         term.clear()
         term.writeln('\x1b[31mGoodbye!\x1b[0m');
         console.error(`Websocket connection closed: ${event.code} ${event.reason}`);
+
+        closeFunc()
     };
 }
 
